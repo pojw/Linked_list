42faee88a0f1124b573c9105ea97ee2236d4fb75
jaylin <pojwmods@gmail.com>
2025-05-27 09:27:54 -0400

    add function added

--- index.js
+++ index.js
@@ -6,30 +6,35 @@ class node {
 }
 
 class LinkedList {
-  constructor(pastNode) {
-    this.pastNode = pastNode;
+  constructor(head) {
+    this.head = head;
 
     //it needs a memeory of two, one to know the previess value, and one to store the next value
   }
   append(node) {
-    if (this.pastNode == null) {
+    if (this.head == null) {
       console.log("first");
-      this.pastNode = node;
+      this.head = node;
+
       return;
-    } else {
-      this.pastNode.next = node.content;
-      console.log(this.pastNode);
-      console.log(node.content);
     }
+    let current = this.head;
+    while (current.next != null) {
+      current = current.next;
+      console.log("1");
+    }
+    current.next = node;
   }
 }
+
 let dog = new node("dog", null);
 let cat = new node("cat", null);
 let human = new node("human", null);
-let mylinkedList = new LinkedList(null, null);
-
+let mylinkedList = new LinkedList(null);
+let fish = new node("fish", null);
 mylinkedList.append(dog);
 
 mylinkedList.append(cat);
-// mylinkedList.append(human);
-console.log(mylinkedList);
+mylinkedList.append(human);
+mylinkedList.append(fish);
+console.log(mylinkedList.head);
